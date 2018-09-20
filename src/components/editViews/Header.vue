51a0485b1b8876056e185e4bbd6152ca5b8c0ea3
showleno <liangzai5555@163.com>
2018-09-20 18:27:09 +0800

    1.增加图片 2.增加fontawsome字体 3.增加elementUi组件库 4.增加css文件夹

---
+++ src/components/editViews/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="header clearfix">
+            <div class="left logo">
+                    <img src="/newpc/images/logo.png" alt="" />
+            </div>
+            <div class="left clearfix">
+                <div class="left btn_group">
+                    <ul>
+                        <li><a><i class="fa fa-floppy-o"></i>保存方案</a></li>
+                        <li><a><i class="fa fa-folder-open"></i>载入方案</a></li>
+                        <li><a><i class="fa fa-eye"></i>预览</a></li>
+                        <li><a><i class="fa fa-refresh"></i>发布</a></li>
+                    </ul>
+                </div>
+            </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+<style>
+    
+</style>
